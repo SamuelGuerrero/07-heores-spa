83f782d152f01c1aa38bb1e4a1c77ad29d482c7d
Samuel Guerrero <guhs990520@gs.utm.mx>
2022-07-14 14:27:33 -0500

    New Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
 import { Heroes } from "./Heroes";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Heroes />
+    <BrowserRouter>
+      <Heroes />
+    </BrowserRouter>
   </React.StrictMode>
 );
