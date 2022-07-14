94894ba29f5afd771a426e1d26449585f2203fae
Samuel Guerrero <guhs990520@gs.utm.mx>
2022-07-14 14:06:49 -0500

    Update

---
+++ src/Heroes.jsx
@@ -0,0 +1,7 @@
+export const Heroes = () => {
+  return (
+    <>
+        <h1>HeroesApp</h1>
+    </>
+  )
+}
---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Heroes } from "./Heroes";
+import "./styles.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Heroes />
+  </React.StrictMode>
+);
